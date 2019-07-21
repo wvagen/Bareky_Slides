5f502c2a5c702de4c7930fda5a4a2729ede2d672
Mouadh Mkadmi <wvagen@gmail.com>
2019-07-21 18:56:01 +0100

    Version 2.0

---
+++ Assets/Scripts/Content.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Content : MonoBehaviour
+{
+    public Transform rotatorAxe;
+
+    void Update()
+    {
+        rotatorAxe.Rotate(-Vector3.forward * Time.deltaTime * 5);
+    }
+
+}
---
+++ Assets/Scripts/PresenationManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresenationManager : MonoBehaviour
+{
+    public Animator myAnim;
+    public GameObject[] Frames;
+
+    int step = 0;
+    void Update()
+    {
+        GetMouseInput();
+    }
+
+
+    void GetMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0) && step < Frames.Length - 1)
+        {
+            DisableAllFrames();
+            step++;
+        }
+        else if (Input.GetMouseButtonDown(1) && step > 0)
+        {
+            DisableAllFrames();
+            step--;
+        }
+        myAnim.SetInteger("Step", step);
+    }
+
+    void DisableAllFrames()
+    {
+        foreach (GameObject f in Frames) f.SetActive(false);
+    }
+
+}
