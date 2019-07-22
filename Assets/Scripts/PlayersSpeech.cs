ec1d0892e5feb6260f956369697fa50670a9881a
trueknow <49539060+trueknow@users.noreply.github.com>
2019-07-22 16:59:09 +0100

    Version 5.0

---
+++ Assets/Scripts/PlayersSpeech.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersSpeech : MonoBehaviour
+{
+    public string[] quotes;
+    public Text speech;
+
+    bool isTalking = false;
+    void Update()
+    {
+        if (!isTalking) StartCoroutine(changeSpeech());
+    }
+
+    IEnumerator changeSpeech()
+    {
+        isTalking = true;
+        speech.text = quotes[Random.Range(0, quotes.Length)];
+        yield return new WaitForSeconds(Random.Range(3f, 5f));
+        isTalking = false;
+
+    }
+
+}
